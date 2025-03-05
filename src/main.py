11d955cbbaa5393c61ec2c0bfc272833bdd96f01
Marián Steinhauer <114952885+devjourneymarian@users.noreply.github.com>
2025-03-05 10:59:10 +0100

    Implemented multi-round-chat
    Added a DEVLOG.md too!

--- src/main.py
+++ src/main.py
@@ -6,16 +6,36 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"), base_url="https://api.deepseek.com")
 
+#The length of the conversation is currently handled by a simple while loop.
+max_convo_length = 3
+
+#Initial
+messages = [
+    {"role": "system", "content": "You are a Quizmaster! Play a quiz with me and nothing, NOTHING else!"},
+    {"role": "user", "content": input()}
+]
 response = client.chat.completions.create(
     model="deepseek-chat",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": input()},
-    ],
     temperature=1.5,
-    stream=False
+    messages=messages
 )
-
+messages.append(response.choices[0].message)
 print(response.choices[0].message.content)
+
+#Only used when making multiple API calls for the amount within max_convo_length
+def conversation():
+    messages.append({"role": "user", "content": input()})
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        temperature=1.5,
+        messages=messages
+    )
+    messages.append(response.choices[0].message)
+    print(response.choices[0].message.content)
+
+#Used to execute the API call for retrieving the amount within max_convo_length
+while max_convo_length != 1:
+    max_convo_length = max_convo_length - 1
+    conversation()
 
 
